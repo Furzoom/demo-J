db42256367fdfcb08e8d4def91525537aa7d8061
furzoom <furzoommn@gmail.com>
2016-05-23 11:16:41 +0800

    add list and clean arguments

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,37 @@
 #!/bin/sh
 
 path="com/furzoom/lab/"
+
+# check argments number
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 [clean | list | <class_name>]"
+    exit 1
+fi
+
+# special arguments
+if [ $1 = "clean" ]
+then
+    rm $path/*.class
+    exit 0
+elif [ $1 = "list" ]
+then
+    ls $path/
+    exit 0
+fi
+
+# get source and object file modify timestamp
 src=${path}$1.java
 obj=${path}$1.class
 
-lastModifySrc=`stat -c %Y $src`
 lastModifyObj=0
+if [ -f $src ]
+then
+    lastModifySrc=`stat -c %Y $src`
+else
+    echo "file '$1' not found"
+    exit 1
+fi
 
 if [ -f $obj ]
 then
@@ -15,7 +41,7 @@ fi
 if [ $lastModifySrc -gt $lastModifyObj ]
 then
     echo "javac com/furzoom/lab/$1.java"
-    javac com/furzoom/lab/$1.java
+    `javac com/furzoom/lab/$1.java`
 fi
 
 if [ -f $obj ]
@@ -27,7 +53,6 @@ if [ $lastModifySrc -gt $lastModifyObj ]
 then
     exit
 fi
-
 
 echo "java com/furzoom/lab/$1"
 java com/furzoom/lab/$1
