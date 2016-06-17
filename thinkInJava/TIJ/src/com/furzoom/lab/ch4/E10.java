fcb89149edfdfd1e490ee56e9a93fedd6574e342
furzoom <mn@furzoom.com>
2016-06-17 15:49:58 +0800

    add E10

--- thinkInJava/TIJ/src/com/furzoom/lab/ch4/E10.java
+++ thinkInJava/TIJ/src/com/furzoom/lab/ch4/E10.java
@@ -22,7 +22,51 @@ public class E10 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for (int i = 1001; i < 10000; ++i) {
+			if (is_vampire(i)) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+	
+	public static boolean is_vampire(int n) {
+		if (n % 100 == 0) {
+			return false;
+		}
+		int[] arr = new int[4];
+		arr[0] = n / 1000;
+		arr[1] = n % 1000 / 100;
+		arr[2] = n % 100 / 10;
+		arr[3] = n % 10;
+		
+		if (n == (arr[0]*10 + arr[1]) * (arr[2]*10 + arr[3])) {
+			return true;
+		} else if (n == (arr[0]*10 + arr[1]) * (arr[3]*10 + arr[2])) {
+			return true;
+		} else if (n == (arr[1]*10 + arr[0]) * (arr[2]*10 + arr[3])) {
+			return true;
+		} else if (n == (arr[1]*10 + arr[0]) * (arr[3]*10 + arr[2])) {
+			return true;
+		} else if (n == (arr[0]*10 + arr[2]) * (arr[1]*10 + arr[3])) {
+			return true;
+		} else if (n == (arr[0]*10 + arr[2]) * (arr[3]*10 + arr[1])) {
+			return true;
+		} else if (n == (arr[2]*10 + arr[0]) * (arr[1]*10 + arr[3])) {
+			return true;
+		} else if (n == (arr[2]*10 + arr[0]) * (arr[3]*10 + arr[1])) {
+			return true;
+		} else if (n == (arr[0]*10 + arr[3]) * (arr[1]*10 + arr[2])) {
+			return true;
+		} else if (n == (arr[0]*10 + arr[3]) * (arr[2]*10 + arr[1])) {
+			return true;
+		} else if (n == (arr[3]*10 + arr[0]) * (arr[1]*10 + arr[2])) {
+			return true;
+		} else if (n == (arr[3]*10 + arr[0]) * (arr[2]*10 + arr[1])) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
