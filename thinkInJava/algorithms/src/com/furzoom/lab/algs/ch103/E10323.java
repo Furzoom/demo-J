4c04f53459a224cebfb14a5d28bc55bf58677742
furzoom <mn@furzoom.com>
2016-10-24 18:01:32 +0800

    E10323

--- thinkInJava/algorithms/src/com/furzoom/lab/algs/ch103/E10323.java
+++ thinkInJava/algorithms/src/com/furzoom/lab/algs/ch103/E10323.java
@@ -1,20 +1,20 @@
 /**
  * Description : 
  * Author      : mn@furzoom.com
- * Date        : Oct 24, 2016 5:58:07 PM
+ * Date        : Oct 24, 2016 6:01:11 PM
  * Copyright (c) 2013-2016, http://furzoom.com All Rights Reserved.
  */
 package com.furzoom.lab.algs.ch103;
 
 /**
- * ClassName    : E10322 <br>
+ * ClassName    : E10323 <br>
  * Function     : TODO ADD FUNCTION. <br>
- * date         : Oct 24, 2016 5:58:07 PM <br>
+ * date         : Oct 24, 2016 6:01:11 PM <br>
  * 
  * @version 
  */
-public class E10322
+public class E10323
 {
-    // t.next = x.next
     // x.next = t
+    // t.next = x.next
 }
