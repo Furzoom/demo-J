237d56fd16a7320a3c9bee4f6345121ecbc6bc42
furzoom <mn@furzoom.com>
2016-10-24 16:13:11 +0800

    E10318

---
+++ thinkInJava/algorithms/src/com/furzoom/lab/algs/ch103/E10318.java
@@ -0,0 +1,20 @@
+/**
+ * Description : 
+ * Author      : mn@furzoom.com
+ * Date        : Oct 24, 2016 4:07:28 PM
+ * Copyright (c) 2013-2016, http://furzoom.com All Rights Reserved.
+ */
+package com.furzoom.lab.algs.ch103;
+
+/**
+ * ClassName    : E10318 <br>
+ * Function     : TODO ADD FUNCTION. <br>
+ * date         : Oct 24, 2016 4:07:28 PM <br>
+ * 
+ * @version 
+ */
+public class E10318
+{
+    // x.next = x.next.next
+    // 删除紧临x后的一个节点
+}
